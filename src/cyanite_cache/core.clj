a978505e76b3b039127ccd62f89a80dafaa06abb
Kirill Kondratenko <cybem@cybem.info>
2014-10-28 14:08:34 +0300

    Simple test

--- src/cyanite_cache/core.clj
+++ src/cyanite_cache/core.clj
@@ -1,7 +1,14 @@
 (ns cyanite-cache.core
-  (:gen-class))
+  (:gen-class)
+  (:require [cyanite-cache.cache :as cache]))
+
+(defn store [tenant period rollup time path data ttl]
+  (println tenant period rollup time path data ttl))
 
 (defn -main
   "Main function."
   [& args]
+  (let [scache (cache/simple-cache store)]
+    (time (cache/put! scache "tenant" 123 60 117293 "my.metric" 10.0 600)))
+  (Thread/sleep (* 190 1000))
   (System/exit 0))
